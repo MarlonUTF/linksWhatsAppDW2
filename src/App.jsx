9192ae7d6b212fb9be4cc8bcecdfd999d7df961e
talissonsidnei <158642399+MarlonUTF@users.noreply.github.com>
2025-09-19 10:14:48 -0300

    Initialize React Vite project with ESLint and MUI
    Set up a new React project using Vite, including configuration files for ESLint and Vite, initial dependencies (MUI, Tailwind, Emotion), and starter files for the app. Updated README with setup and usage instructions.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
