9eacff31a73322bf215aaec3d4f1f2304e66dd03
alduin10 <ryantomaztalavieira@gmail.com>
2025-10-16 15:20:22 -0300

    save

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/linksWhatsAppDW2/',
+  base: '/',
   build: {
     minify: 'terser', // Força o uso do Terser
     terserOptions: {
