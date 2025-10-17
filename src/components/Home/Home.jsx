2e2e950363c1562a6e1f803410d932747b6c3407
talissonsidnei <158642399+MarlonUTF@users.noreply.github.com>
2025-10-17 09:02:59 -0300

    Set Home layout to column flex direction
    Added 'flex-col' to the Home component's container div to arrange child components vertically instead of the default row direction.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import Main from "./Corpo/Main";
 export default function Home() {
   return (
     <div className=" min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 p-4  ">
-      <div className="flex max-w-4xl mx-auto justify-center align-center ">
+      <div className="flex flex-col max-w-4xl mx-auto justify-center align-center ">
         <Header />
         <Main />
       </div>
